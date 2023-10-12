d54b90de88226bf7631b943737cba4f6992e2b8c
Hakuin123 <HK256@qq.com>
2023-10-12 19:19:29 +0800

    1

--- themes/frame/source/js/search.js
+++ themes/frame/source/js/search.js
@@ -51,11 +51,11 @@ document.addEventListener('DOMContentLoaded', () => {
       container.innerHTML = `<div class="search-result-message" ></div>`
     } else if (resultItems.length === 0) {
       // no result
-      container.innerHTML = `<div class="search-result-message" >No result found</div>`;
+      container.innerHTML = `<div class="search-result-message" >没找到诶…</div>`;
     } else {
       // display result(s)
       container.innerHTML = `
-      <div class="search-result-message">${resultItems.length} result(s) found</div>
+      <div class="search-result-message">找到了 ${resultItems.length} 篇！</div>
       <ul class="search-result-list">${resultItems.map(result => result.item).join('<div class="h-line-secondary"></div>')}
       </ul>`;
     }
